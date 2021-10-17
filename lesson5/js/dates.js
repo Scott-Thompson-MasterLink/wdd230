98108b79a0a9e8f07ba568a06337cd8e5b1e67cf
Scott-Thompson-MasterLink <Tho21034@byui.edu>
2021-10-16 21:56:45 -0600

    lesson 5 final 1

--- lesson5/js/dates.js
+++ lesson5/js/dates.js
@@ -3,3 +3,13 @@ document.querySelector(".copyrightyear").textContent = new Date().getFullYear();
 const today = new Date();
 const options = { weekday: 'long', day: 'numeric' , month: 'long', year: 'numeric'};
 document.querySelector(".todayDate").textContent = today.toLocaleDateString(undefined, options)
+
+
+const pancake_banner = document.querySelector('.pancake_banner')
+console.log(today.day)
+if (today.getDay() == 5){
+    pancake_banner.style.display = "block";
+}
+else{
+    pancake_banner.style.display = "none";
+};
